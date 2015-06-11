d74f4d9895444d7dd93b4ea93e9f7792b9d29e9f
Andrew Koo <andrewkoo@berkeley.edu>
2015-06-11 04:01:43 -0700

    bars

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 // Constants
 var margin = {top: 50, right: 50, bottom: 50, left: 50},
     width = 800 - margin.left - margin.right,
-    height = 800 - margin.top - margin.bottom,
-    legendWidth = 300;
+    height = 500 - margin.top - margin.bottom,
 
     // Global variables
     data = [];
     csv = [];
     accumulatedData = [];
-    currYear = 0;
-    legendInfo =-1;
+    iteration = 0;
+    logScale = 0;
+
+// Tooltip
+var tooltip = d3.select("body")
+	.append("div")
+	.style("position", "absolute")
+	.style("z-index", "10")
+	.style("visibility", "hidden")
+    .attr("class", "tooltip")
+
+function updateTooltip(d) {
+    return (
+        tooltip
+            .text( d.Tournament + ": $" + d.Prize.toString() )
+            .style("visibility", "visible")
+    );
+}
+
 
 
 // Axes and Color Scales
 var x = d3.time.scale()
     .range([0, width]);
-var y = d3.scale.linear()
+var yLog = d3.scale.log()
+    .range([height, 0]);
+var yLinear = d3.scale.linear()
     .range([height, 0]);
 var r = d3.scale.linear()
-    .range([3, 50]);
+    .range([3, 30]);
+var y = yLinear;
+
+
 var xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom")
@@ -29,30 +50,13 @@ var yAxis = d3.svg.axis()
     .orient("left")
     .tickFormat(d3.format(".2s"));
 
+
 // Initialize SVG
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// Initialize Legend
-var legend = d3.select("body").append("svg")
-    .attr("width", legendWidth + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .attr("transform", "translate(" + margin.left + "," +2 * margin.top + ")")
-    .append("p")
-    .attr("class", "legendinfo");
-
-
-
-function updateLegend(d) {
-    console.log(d.Tournament);
-    legend
-        .select("legendinfo")
-        .text(d.Tournament)
-       
-}
 
 // Gradient
 var gradient = svg
@@ -72,6 +76,7 @@ gradient
     .attr("offset", ".3")
     .attr("stop-color", "#a9100d");
 
+
 // Load data
 d3.csv("data.csv", function(csv) {
     csv.forEach(function(d) {
@@ -83,7 +88,7 @@ d3.csv("data.csv", function(csv) {
     });
     // Clean data: filter qualifier tournaments
     function filterByPrize(obj) {
-        if (obj.Prize === "0") {
+        if (obj.Prize == "0") {
             return false;
         } else {
             return true;
@@ -98,12 +103,14 @@ d3.csv("data.csv", function(csv) {
         .entries(csv);
 
     // Start program on first year
-    csv = data[currYear].values;
+    csv = data[iteration].values;
 
     // Axes
     x.domain(d3.extent(csv, function(d) { return d.Date; })).nice();
-    y.domain([0, d3.max(csv, function(d) { return d.Prize; })]).nice();
+    yLinear.domain([0, d3.max(csv, function(d) { return d.Prize; })]).nice();
     r.domain([0, d3.max(csv, function(d) { return d.Prize; })]);
+    yLog.domain([d3.min(csv, function(d) { return d.Prize; }), d3.max(csv, function(d) { return d.Prize; })]).nice();
+    
 
     svg.append("g")
         .attr("class", "x axis")
@@ -128,25 +135,71 @@ d3.csv("data.csv", function(csv) {
         .attr("cx", function(d) { return x(d.Date); })
         .attr("cy", function(d) { return y(d.Prize); })
         .attr("fill", "url(#gradient)")
-        .on("mouseover", function(d) {
-            updateLegend(d);
-        });
+        .on("mouseover", function(d) { updateTooltip(d); } )
+        .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+        .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 });
 
 
 // ** Update data section (Called from the onclick)
 function updateData() {
 
-    if (currYear == 0) {
-        allData = csv = data[currYear].values;
+    if (iteration == 0) {
+        allData = csv = data[iteration].values;
     }
-    currYear++;
-    if (currYear < 5) {
-        csv = data[currYear].values
+    iteration++;
+    if (iteration < 5) {
+        csv = data[iteration].values
         allData = allData.concat(csv);
-    } else {
-        console.log("Done");
-        return;
+    } 
+    if (iteration == 5) { // Log
+        csv = [];
+        y = yLog;
+        yAxis.scale(y).tickValues([200, 2000, 20000, 200000, 2000000, 20000000]);
+    }
+    
+    if (iteration > 5) { // Sums
+        svg.selectAll("circle.prev.dot")
+            .transition()
+            .duration(750)
+            .attr("cy", height)
+            .attr("r", 0);
+        
+        var sums = data.map(function(arr) {
+            sum = 0;
+            for (var i = 0; i < arr.values.length; i++) {
+                sum += arr.values[i].Prize;
+            }
+            return sum;
+        });
+        
+//        y = yLinear;
+//        yAxis.scale(y).tickValues([1]);
+////        
+//
+//        d3.select(".x.axis").remove();
+//        var format = d3.time.format("%m/%d/Y");
+//        x.domain([format.parse("01/01/2011"), format.parse("01/01/2015")])
+//        xAxis = d3.svg.axis()
+//            .scale(x)
+//            .tickFormat("%Y")
+//            .orient("bottom");            
+        
+//        svg.append("g")
+//            .attr("class", "x axis")
+//            .attr("transform", "translate(0," + height + ")")
+//            .call(xAxis);
+//        
+//        svg.selectAll(".bar")
+//            .data(sums)
+//            .enter().append("rect")
+//            .attr("class", "bar")
+//            .attr("x", function(i) { return i } )//x(format("01/01/2015")) )
+//            .attr("width", (width - margin.right) / 5)
+//            .attr("y", function(d) { return y(d);} )
+//            .attr("height", function(d) { return height - y(d); });
+        
+        return;        
     }
     
     // Scatterplot
@@ -161,16 +214,16 @@ function updateData() {
         .attr("cy", function(d) { return y(d.Prize); })
         .attr("fill", "url(#gradient)")
         .attr("cx", width + margin.right * 2)
-        .attr("fill-opacity", 0)
-        .on("mouseover", function(d) {
-            updateLegend(d);
-        })
+        .on("mouseover", function(d) { updateTooltip(d); } )
+        .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+        .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
     
     // Axes
     x.domain(d3.extent(allData, function(d) { return d.Date; })).nice();
-    y.domain([0, d3.max(allData, function(d) { return d.Prize; })]).nice();
+    yLinear.domain([0, d3.max(allData, function(d) { return d.Prize; })]).nice();
     r.domain([0, d3.max(allData, function(d) { return d.Prize; })]);
-
+    yLog.domain([d3.min(allData, function(d) { return d.Prize; }), d3.max(allData, function(d) { return d.Prize; })]).nice();
+    
     svg.transition()
         .select(".x.axis")
         .duration(750)
@@ -194,6 +247,5 @@ function updateData() {
         .attr("cx", function(d) { return x(d.Date); })
         .attr("r", function(d) { return r(d.Prize); })
         .attr("cy", function(d) { return y(d.Prize); })
-        .attr("fill-opacity", 1);
                 
 }
