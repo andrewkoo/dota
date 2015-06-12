53fe18c205582580993579933b934c67c13488ad
andrewkoo <andrewkoo@berkeley.edu>
2015-06-12 16:07:09 -0700

    still need to clean up prev functio

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // Constants
 var margin = {top: 50, right: 50, bottom: 50, left: 50},
-    width = 800 - margin.left - margin.right,
-    height = 800 - margin.top - margin.bottom,
+    width = 900 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom,
     csv = [], // Nested data (index by year)
-    iteration = 0,
-    prevData = [],
     currData = [],
-    largestIteration = 0,
+    sums = [],
+    iteration = 0,
+    largestIteration = [true, true, true, true, true, true],
     caller = -1; // Increment and decrement flag
-    
+var parseDate = d3.time.format("%m/%d/%y").parse;    
+
 // Tooltip
 var tooltip = d3.select("body")
 	.append("div")
@@ -45,6 +46,32 @@ var yAxis = d3.svg.axis()
     .orient("left")
     .tickFormat(d3.format(".2s"));
 
+// Initialize Tracking Bar
+var labels = ["2011", "2012", "2013", "2014", "2015", "Log", "Totals"];
+var trackScale = d3.scale.linear()
+    .range([0, width])
+    .domain([0, 7]);
+var trackingBar = d3.svg.axis()
+    .scale(trackScale)
+    .orient("bottom")
+    .tickFormat(function(i) { return labels[i]; });
+var tracker = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", 30)
+tracker.append("g")
+    .attr("transform", "translate(" + margin.left + ", 10 )")
+    .attr("class", "trackingAxis")
+    .call(trackingBar);
+tracker.selectAll("text")
+    .attr("transform", "translate(" + (width / 7)/2 + ", 0 )");
+tracker.append("rect")
+    .attr("x", margin.left)
+    .attr("y", 8)
+    .attr("width", trackScale(1))
+    .attr("height", 5)
+    .attr("fill", "#555")
+    .attr("opacity", .75);
+    
 // Initialize SVG
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -67,14 +94,13 @@ gradient
     .attr("stop-color", "#555");
 gradient
     .append("stop")
-    .attr("offset", ".3")
+    .attr("offset", "1")
     .attr("stop-color", "#a9100d");
 
 // Load data
 d3.csv("data.csv", function(data) {
     data.forEach(function(d) {
         // "Tournament","Start","End","Location","Prize","Winner","Runner-Up"
-        var parseDate = d3.time.format("%m/%d/%y").parse;
         d.Date = parseDate(d.Start);
         d.Year = d.Date.getFullYear();
         d.Prize = Math.round(d.Prize); // Convert string to int
@@ -93,13 +119,24 @@ d3.csv("data.csv", function(data) {
         .key(function(d) { return d.Year; })
         .sortKeys(d3.ascending)
         .entries(data);
+
+    // Calculate sums and years
+    sums = csv.map(function(arr, i) {
+        sum = 0;
+        for (var i = 0; i < arr.values.length; i++) {
+            sum += arr.values[i].Prize;
+        }
+        return {sum: sum, year: d3.time.format("%Y").parse(arr.key)};
+    });
+    
+    
     // Start program on first year
     currData = csv[iteration].values;
 
     // Axes
     yLinear.domain([0, d3.max(currData, function(d) { return d.Prize; })]).nice();
     yLog.domain([d3.min(currData, function(d) { return d.Prize; }), d3.max(currData, function(d) { return d.Prize; })]).nice();
-    x.domain([d3.time.format("%m/%d/%y").parse("08/01/11"), d3.max(currData, function(d) { return d.Date; })]).nice();
+    x.domain([d3.time.format("%m/%d/%y").parse("01/01/11"), d3.max(currData, function(d) { return d.Date; })]).nice();
     r.domain([0, d3.max(currData, function(d) { return d.Prize; })]);
     
     svg.append("g")
@@ -110,7 +147,7 @@ d3.csv("data.csv", function(data) {
         .attr("class", "y axis")
         .call(yAxis)
         .append("text")
-        .attr("transform", "rotate(-90)")
+        .attr("transform", "translate(0," + "-40" + ")")
         .attr("y", 6)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
@@ -130,12 +167,21 @@ d3.csv("data.csv", function(data) {
         .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 });
 
-
-
 function updateDots() {
     if (iteration == 5) { // Log
+        if (caller == 0) {
+            svg.selectAll(".bar")
+                .transition()
+                .duration(750)
+                .attr("height", 0)
+                .attr("y", height );  
+            xAxis.tickFormat(d3.time.format("%m/%Y"));
+        } 
         y = yLog;
         yAxis.scale(y).tickValues([200, 2000, 20000, 200000, 2000000, 20000000]);
+    }  else {
+        y = yLinear;
+        yAxis.scale(y).tickValues(null);
     }
     
     // Set all existing dots to prev class
@@ -143,7 +189,8 @@ function updateDots() {
         .attr("class", "prev dot");
     
     // Initialize new dots according to old axes
-    if ((iteration < 5) && (iteration > largestIteration)) { // <----- THIS PART
+    if ((iteration < 5) && (largestIteration[iteration-1])) {
+        largestIteration[iteration-1] = false;
         var curr_dots = svg.selectAll( "curr dot" )
             .data(csv[iteration].values)
             .enter().append("circle")
@@ -158,7 +205,7 @@ function updateDots() {
     }
     
     // Update axes with new data
-    x.domain([d3.time.format("%m/%d/%y").parse("08/01/11"), d3.max(currData, function(d) { return d.Date; })]).nice();
+    x.domain([d3.time.format("%m/%d/%y").parse("01/01/11"), d3.max(currData, function(d) { return d.Date; })]).nice();
     yLinear.domain([0, d3.max(currData, function(d) { return d.Prize; })]).nice();
     r.domain([0, d3.max(currData, function(d) { return d.Prize; })]);
     yLog.domain([d3.min(currData, function(d) { return d.Prize; }), d3.max(currData, function(d) { return d.Prize; })]).nice();
@@ -175,17 +222,22 @@ function updateDots() {
     svg.selectAll( "circle.prev.dot" )
         .transition()
         .duration(750)
+        .each( function()  {
+            if (caller == 0 && iteration == 5) {
+                return svg.selectAll ( "circle.prev.dot" ).transition().duration(750).delay(750);
+            }
+        })
         .attr("r", function(d) { return r(d.Prize); })
         .attr("cx", function(d) { return x(d.Date); })
         .attr("cy", function(d) { return y(d.Prize); })
-        .attr("fill", "url(#gradient)");
+        
     if ((iteration < 5) && (caller == 1)) {
         curr_dots
             .transition()
             .duration(750)
             .attr("cx", function(d) { return x(d.Date); })
             .attr("r", function(d) { return r(d.Prize); })
-            .attr("cy", function(d) { return y(d.Prize); })
+            .attr("cy", function(d) { return y(d.Prize); });
     }
 }
 
@@ -197,40 +249,30 @@ function updateBars() {
             .attr("cy", height)
             .attr("r", 0);
 
-        var parseDate = d3.time.format("%m/%d/%y").parse;
-        
-        // Calculate sums and years
-        var sums = csv.map(function(arr, i) {
-            sum = 0;
-            for (var i = 0; i < arr.values.length; i++) {
-                sum += arr.values[i].Prize;
-            }
-            return {sum: sum, year: d3.time.format("%Y").parse(arr.key)};
-        });
-        
-        
-        x.domain([ parseDate("01/01/10") , parseDate("01/01/16") ])
-            .range([0, width]);
-        xAxis.scale(x).tickFormat(d3.time.format("%Y"));
-        
-        svg.transition()
-            .select(".x.axis")
-            .duration(750)
-            .style("stroke", 0)
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis)
-        
-        d3.select(svg.selectAll(".tick")[0][0]).attr("visibility","hidden");
-        d3.select(svg.selectAll(".tick")[0][6]).attr("visibility","hidden");
-        
-        y = yLinear;
-        y.domain([0, d3.max(sums, function(d) { return d.sum; })]).nice()
-        yAxis.scale(y).tickValues(null);
-        svg.transition()
-            .select(".y.axis")
-            .duration(750)
-            .call(yAxis)
-        
+    x.domain([ parseDate("01/01/10") , parseDate("01/01/16") ])
+        .range([0, width]);
+    xAxis.scale(x).tickFormat(d3.time.format("%Y"));
+
+    svg.transition()
+        .select(".x.axis")
+        .duration(750)
+        .style("stroke", 0)
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+
+    d3.select(svg.selectAll(".tick")[0][0]).attr("visibility","hidden");
+    d3.select(svg.selectAll(".tick")[0][6]).attr("visibility","hidden");
+
+    y = yLinear;
+    y.domain([0, d3.max(sums, function(d) { return d.sum; })]).nice()
+    yAxis.scale(y).tickValues(null);
+    svg.transition()
+        .select(".y.axis")
+        .duration(750)
+        .call(yAxis)
+
+    if (largestIteration[iteration-1]) {
+        largestIteration[iteration-1] = false;
         svg.selectAll(".bar")
             .data(sums)
             .enter().append("rect")
@@ -250,6 +292,15 @@ function updateBars() {
             .delay(750)
             .attr("y", function(d) { return y(d.sum); } )    
             .attr("height", function(d) { return height - y(d.sum); });
+    } else {
+        svg.selectAll(".bar")
+            .data(sums)
+            .transition()
+            .duration(750)
+            .delay(750)
+            .attr("y", function(d) { return y(d.sum); } )
+            .attr("height", function(d) { return height - y(d.sum); } );
+    }
 }
 
 function concatData(iter) {
@@ -261,22 +312,23 @@ function concatData(iter) {
 }
 
 function processData(caller) {
-    if (iteration == 0) {
-        prevData = currData = csv[0].values;
-    } else if (iteration >= csv.length - 1) {
-        prevData = currData = concatData(csv.length - 1);
+    if (iteration >= 6) {
+        iteration = 6;
+    } else if (iteration <= 0) {
+        iteration = 0;
     }
-    
-    else if (caller) {
-        currData = concatData(iteration);
-        prevData = concatData(iteration - 1);
-    } else {
-        currData = concatData(iteration);
-        prevData = concatData(iteration + 1);
-    }
+    currData = concatData(iteration);
+}
+
+function updateTracker() {
+    tracker.select("rect")
+        .transition()
+        .duration(750)
+        .attr("width", trackScale(iteration+1));
 }
 
 function controller() {
+    updateTracker();
     if (iteration <= 5) {
         updateDots();
     }
